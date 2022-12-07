538fa92f4b2d32107d01403ab6685150586ddb85
btilling <54737272+bentilling@users.noreply.github.com>
2022-12-07 16:30:00 +0000

    small day7 refactor

--- src/main/kotlin/Day7.kt
+++ src/main/kotlin/Day7.kt
@@ -1,71 +1,64 @@
-import java.util.Dictionary
-
 fun main() {
     val inputFile = "day7/commands"
     val input = readInputAsList(inputFile)
     val inputTest = readInputAsList("$inputFile-test")
+
+    val totalsTest = parseInputToNodes(inputTest).map {sum(it) }
+
+    check(task1(totalsTest) == 95437)
+    check(task2(totalsTest) == 24933642)
+
     val totals = parseInputToNodes(input).map {sum(it) }
+
+    println(task1(totals))
+    println(task2(totals))
+
+}
+
+fun task1(totals: List<Int>): Int {
+    return totals.filter { it <= 100000 }.sum()
+}
+
+fun task2(totals: List<Int>): Int {
     val neededSpace = 30000000 - (70000000 - totals.max())
-    println(totals.filter { it >= neededSpace }.min())
+    return totals.filter { it >= neededSpace }.min()
 }
 
 fun parseInputToNodes(input: List<String>): List<Directory> {
-    val root = Directory("/", null, mutableListOf(), mutableListOf())
+    val root = Directory("/", null, mutableListOf())
     val directories = mutableListOf(root)
     var currentDirectory = root
     input.forEach {
-        println(it)
-        if (it.contains("\$ cd ..")) {
-            println("up from $currentDirectory to ${currentDirectory.parent!!}")
+        if (it == "\$ cd ..") {
             currentDirectory = currentDirectory.parent!!
-        } else if (it.contains("\$ cd ")) {
+        } else if (it.startsWith("\$ cd ")) {
             val directoryName = it.split("\$ cd ")[1]
             val existingDir = currentDirectory.children.find { dir ->
                 dir.name == directoryName
             } ?: root
-            println("moving to directory: $existingDir")
             currentDirectory = existingDir
         } else if (it.startsWith("dir")) {
             val directoryName = it.split("dir ")[1]
-            println("creating new directory: $directoryName")
-            val newDir = Directory(
-                directoryName,
-                currentDirectory,
-                mutableListOf(),
-                mutableListOf()
-            )
+            val newDir = Directory(directoryName, currentDirectory, mutableListOf())
             directories.add(newDir)
             currentDirectory.children.add(newDir)
         } else if (!it.startsWith("\$")) {
-            val fileParts = it.split(" ")
-            println("adding file ${fileParts[0].toInt()}")
-            currentDirectory.files.add(fileParts[0].toInt())
+            currentDirectory.size += it.split(" ")[0].toInt()
         }
     }
     return directories
 }
 
-
 class Directory(
     val name: String,
     val parent: Directory?,
     val children: MutableList<Directory>,
-    val files: MutableList<Int>) {
-
-    fun size() = this.files.sum()
-
-    override fun toString(): String {
-        return "Directory $name"
-    }
-}
+    var size: Int = 0) {}
 
 fun sum(dir: Directory): Int {
-    println(dir)
     return if (dir.children.isEmpty()) {
-        println("$dir: no children, ${dir.size()}")
-        dir.size()
+        dir.size
     } else {
-        println("$dir: children: ${dir.children} + ${dir.size()}")
-        dir.children.sumOf { sum(it) } + dir.size()
+        dir.children.sumOf { sum(it) } + dir.size
     }
 }
